545146e8a1c42835995499a54c19b4522e28c7e3
Alexander Hoischen <alexander.hoischen@gmx.net>
2017-09-30 18:48:16 +0200

    Insert hard coded user mappings into database

--- src/user_mappings_hack.js
+++ src/user_mappings_hack.js
@@ -1,9 +1,14 @@
 'use strict';
 
+const fsExtra = require('fs-extra');
+
 function insertHardcodedMappings(database) {
-    
+    const userMappings = fsExtra.readJSONSync('./accounts.json');
+    for (const userMapping in userMappings) {
+        database.user.add(userMapping);
+    }
 }
 
 module.exports = {
-    insertHardcodedMappings: insertHardcodedMappings(),
+    insertHardcodedMappings: insertHardcodedMappings,
 }
